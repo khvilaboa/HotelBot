59fe6cf7c0b5dabc938d9ab87039458165630551
khvilaboa <khvilaboa@esei.uvigo.es>
2017-04-21 23:59:43 +0200

    Added type of room in DB initialization

--- tools/init_db.py
+++ tools/init_db.py
@@ -26,10 +26,13 @@ rooms.remove()
 print("\nIntroduciendo datos del hotel...")
 hotels.insert_one({"room_prices": {"individual": 20, "double": 35, "suite": 60}})
 
+room_types = ("individual", "double", "suite") 
+ind = 0
 print("\nIntroduciendo datos de las habitaciones...")
 for floor in range(1, NUM_FLOORS + 1):
 	for room in range(1, NUM_ROOMS_PER_FLOOR + 1):
-		rooms.insert({"floor": floor, "number": room})
+		rooms.insert({"floor": floor, "number": room, "type": room_types[ind]})
+		ind = (ind + 1) % len(room_types)
 
 print("\n\nBase de datos iniciada con exito :)")
 raw_input()
