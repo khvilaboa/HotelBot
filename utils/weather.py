36cfabb09394df3421ae66cc6665db12edf67aa5
Iván Martínez Mateu <imm@ivanmartinezmateu.com>
2017-05-21 15:29:23 +0200

    Avoid using shadow built-in names

--- utils/weather.py
+++ utils/weather.py
@@ -26,34 +26,34 @@ class Weather:
     def get_fahrenheit_temperature(self, address):
         return self.owm.weather_at_place(address).get_weather().get_temperature('fahrenheit')
 
-    def get_current_weather(self, lat, long):
+    def get_current_weather(self, lat, lon):
         geolocator = geopy.Nominatim()
-        location = geolocator.reverse(lat, long)
+        location = geolocator.reverse(lat, lon)
         return self.owm.weather_at_place(location.address).get_weather()
 
-    def get_daily_forecast(self, lat, long):
+    def get_daily_forecast(self, lat, lon):
         geolocator = geopy.Nominatim()
-        location = geolocator.reverse(lat, long)
+        location = geolocator.reverse(lat, lon)
         return self.owm.daily_forecast(location.address)
 
-    def get_wind(self, lat, long):
+    def get_wind(self, lat, lon):
         geolocator = geopy.Nominatim()
-        location = geolocator.reverse(lat, long)
+        location = geolocator.reverse(lat, lon)
         return self.owm.weather_at_place(location.address).get_weather().get_wind()
 
-    def get_humidity(self, lat, long):
+    def get_humidity(self, lat, lon):
         geolocator = geopy.Nominatim()
-        location = geolocator.reverse(lat, long)
+        location = geolocator.reverse(lat, lon)
         return self.owm.weather_at_place(location.address).get_weather().get_humidity()
 
-    def get_celsius_temperature(self, lat, long):
+    def get_celsius_temperature(self, lat, lon):
         geolocator = geopy.Nominatim()
-        location = geolocator.reverse(lat, long)
+        location = geolocator.reverse(lat, lon)
         return self.owm.weather_at_place(location.address).get_weather().get_temperature('celsius')
 
-    def get_fahrenheit_temperature(self, lat, long):
+    def get_fahrenheit_temperature(self, lat, lon):
         geolocator = geopy.Nominatim()
-        location = geolocator.reverse(lat, long)
+        location = geolocator.reverse(lat, lon)
         return self.owm.weather_at_place(location.address).get_weather().get_temperature('fahrenheit')
 
 
