1a5bb944d796eaaacebc98ac802236545c222cac
khvilaboa <khvilaboa@esei.uvigo.es>
2017-05-28 12:22:05 +0200

    Improved dateparser
    "el [measure] que viene"
    "en [measure] dias"
    "del [number] al [number] de [month]"

--- utils/dateparser.py
+++ utils/dateparser.py
@@ -14,10 +14,12 @@ rel_days = "anteayer|ayer|pasado manana|manana"
 
 measures = "dia|semana|mes|ano"
 
-regex1 = re.compile("((%s|[0-9]+)\s+de\s+(%s))" % (numbers, months), re.IGNORECASE)
+regex1 = re.compile("((%s|[0-9]+)\s+al\s+(%s|[0-9]+)\s+de\s+(%s))" % (numbers, numbers, months), re.IGNORECASE)
+regex2 = re.compile("((%s|[0-9]+)\s+de\s+(%s))" % (numbers, months), re.IGNORECASE)
 # regex2 = re.compile("(([0-9]+)\s+de\s+(%s))" % (months), re.IGNORECASE)
-# regex3 = re.compile("(el|la)\s+(%s)\s+que\s+viene" % (measures), re.IGNORECASE)
-regex2 = re.compile("(dentro\s+de\s+(%s|[0-9]+)s?\s+(%s)s?)" % (numbers, measures), re.IGNORECASE)
+regex3 = re.compile("((dentro\s+de|en)\s+(%s|[0-9]+)s?\s+(%s)s?)" % (numbers, measures), re.IGNORECASE)
+regex4 = re.compile("((el|la)\s+(%s)\s+que\s+viene)" % measures, re.IGNORECASE)
+
 
 numbersLst = numbers.split("|")
 daysLst = days.split("|")
@@ -33,6 +35,20 @@ def parse(text):
 
     matches = regex1.findall(text)
     for data in matches:
+        match, numberStart, numberEnd, month = data
+        numberStart = max(1, numbersLst.index(numberStart) - 1) if numberStart in numbersLst else int(numberStart)
+        numberEnd = max(1, numbersLst.index(numberEnd) - 1) if numberEnd in numbersLst else int(numberEnd)
+        month = monthsLst.index(month) + 1
+        newStart = datetime(now.year, month, numberStart)
+        newEnd = datetime(now.year, month, numberEnd)
+        if newStart < now:
+            newStart = newStart.replace(year=newStart.year + 1)
+        if newEnd < now:
+            newEnd = newEnd.replace(year=newEnd.year + 1)
+        text = text.replace(match, "%s al %s" % (newStart.strftime("%d/%m/%Y"), newEnd.strftime("%d/%m/%Y")))
+
+    matches = regex2.findall(text)
+    for data in matches:
         match, number, month = data
         number = max(1, numbersLst.index(number) - 1) if number in numbersLst else int(number)
         month = monthsLst.index(month) + 1
@@ -41,9 +57,9 @@ def parse(text):
             new = new.replace(year=new.year + 1)
         text = text.replace(match, new.strftime("%d/%m/%Y"))
 
-    matches = regex2.findall(text)
+    matches = regex3.findall(text)
     for data in matches:
-        match, number, measure = data
+        match, _, number, measure = data
         number = max(1, numbersLst.index(number) - 1) if number in numbersLst else int(number)
         new = now
         if measure == "dia":
@@ -59,6 +75,24 @@ def parse(text):
                 new = now.replace(month=1 + number - 12 * (number / 12))
         elif measure == "ano":
             new = now.replace(year=now.year + number)
+        text = text.replace(match, "el %s" % new.strftime("%d/%m/%Y"))
+
+    matches = regex4.findall(text)
+    for data in matches:
+        match, _, measure = data
+        new = now
+        if measure == "dia":
+            new += timedelta(days=1)
+        elif measure == "semana":
+            new += timedelta(days=7)
+        elif measure == "mes":
+            if now.month <= 11:
+                new = now.replace(month=now.month + 1)
+            else:
+                new = now.replace(year=now.year + 1)
+                new = now.replace(month=1)
+        elif measure == "ano":
+            new = now.replace(year=now.year + 1)
         text = text.replace(match, "el %s" % new.strftime("%d/%m/%Y"))
 
     for rel_day in rel_daysLst:
